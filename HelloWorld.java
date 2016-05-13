bd2a35e24740284ae8fc907f9d34dc83394f4c02
uchy4 <uch14002@byui.edu>
2016-05-13 11:51:59 -0600

    First commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld{
+
+
+void display(){
+
+System.out.println("Hello Github World!");
+}
+public static void main(String[] args){
+
+HelloWorld h = new HelloWorld();
+h.display();
+}
+}
